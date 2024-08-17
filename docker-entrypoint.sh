5d03dc0af6e43bf70b06f19569d0599adab7702b
SerhiiLakodei <>
2024-08-17 18:02:49 +0300

    add telegram button

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,2 @@
 alembic upgrade head
-gunicorn -k uvicorn.workers.UvicornWorker -c gunicorn-conf.py server:app
+gunicorn -k uvicorn.workers.UvicornWorker -c gunicorn-conf.py server:app --timeout 120
